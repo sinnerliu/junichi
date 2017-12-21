37efbeff80a34a5b043d45f95c6bdec275bd9176
liuyan <liuyan@asiainfo.com>
2017-12-21 17:08:57 +0800

    fix

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
 </div>
 <?php $this->footer(); ?>
 
+
 <script src="<?php $this->options->themeUrl('js/functions.js'); ?>"></script>
 <?php if(!empty($this->options->search_form) && in_array('Pjax', $this->options->search_form)): ?>
 <script src="<?php $this->options->themeUrl('js/prism.js'); ?>" data-no-instant></script>
