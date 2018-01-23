33744e76e718e2bf7a0181d17fb8f977f4aa0df6
liuyan <liuyan@asiainfo.com>
2018-01-23 18:31:06 +0800

    fix

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
 		<footer id="footer" class="inner">
-		<span>博客已运行</span><span id="span_dt_dt"></span><br>
-			&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a> 由 <a href="http://www.typecho.org" target="_blank">Typecho</a> 强力驱动 Theme is <a href="https://uefeng.com" target="_blank">Junichi</a>
+          <?php Typecho_Widget::widget('Widget_Stat')->to($stat); ?>
+           <span>博客已运行</span><span id="span_dt_dt"></span>&nbsp;&nbsp; <br>
+<p><?php _e('统计日期：%s  分类：<strong>%s</strong>　文章：<strong>%s</strong>篇　评论 <strong>%s</strong>条',date('n月j日'), $stat->categoriesNum, $stat->publishedPostsNum, $stat->publishedCommentsNum); ?></p>
+			&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a> 由 <a href="http://www.typecho.org" target="_blank">Typecho</a> 强力驱动 Theme by <a href="https://uefeng.com" target="_blank">Junichi</a>&nbsp;&nbsp;
+         <br><a href='http://www.miibeian.gov.cn' target='_blank'> </a>&nbsp;&nbsp;
 		</footer>
 	</div>
 </div>
 <?php $this->footer(); ?>
-
 
 <script src="<?php $this->options->themeUrl('js/functions.js'); ?>"></script>
 <?php if(!empty($this->options->search_form) && in_array('Pjax', $this->options->search_form)): ?>
@@ -20,6 +22,8 @@
 	});
 	InstantClick.init();
 </script>
+
+
 <?php else : ?>
 <script src="<?php $this->options->themeUrl('js/prism.js'); ?>"></script>
 <?php endif; ?>
--- js/functions.js
+++ js/functions.js
@@ -53,3 +53,30 @@ jQuery(document).ready(function($) {
         );
     });
 });
+
+ <!--百度统计-->
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?66a88d229a267d166842ac455f89ee20";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+<!--百度推送-->
+
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+
+  
