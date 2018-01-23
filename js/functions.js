6efed68b3bde21a7f1c640f80e1ade35e4af510b
liuyan <liuyan@asiainfo.com>
2018-01-23 18:35:52 +0800

    fix

--- js/functions.js
+++ js/functions.js
@@ -31,6 +31,7 @@ br.show('wechat-toggle', 'wechat-show');
 function show_date_time(){window.setTimeout("show_date_time()",1e3);var BirthDay=new Date("2015/06/06"),today=new Date,timeold=today.getTime()-BirthDay.getTime(),msPerDay=864e5,e_daysold=timeold/msPerDay,daysold=Math.floor(e_daysold),e_hrsold=24*(e_daysold-daysold),hrsold=Math.floor(e_hrsold),e_minsold=60*(e_hrsold-hrsold),minsold=Math.floor(60*(e_hrsold-hrsold)),seconds=Math.floor(60*(e_minsold-minsold));span_dt_dt.innerHTML=daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒";}
 show_date_time();
 
+/*
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e){
@@ -53,8 +54,9 @@ jQuery(document).ready(function($) {
         );
     });
 });
+*/
 
- <!--百度统计-->
+<!--百度统计-->
 
 var _hmt = _hmt || [];
 (function() {
